4d18c77a22ce741adab69f8f1552cd48f854556b
liufrancois <96915859+liufrancois@users.noreply.github.com>
2022-01-11 15:51:01 +0100

    plan
    plan étape par étape

---
+++ MH.py
@@ -0,0 +1,8 @@
+#choisir aleatoirement la place de la voiture dans une liste de 3 elements
+#demander a l utilisateur quel porte il choisi
+#ouvrir une autre porte avec une chevre deriere
+#demander si l utilisateur veux changer de porte
+#en fonction de la reponce lui changer de porte
+#ouvrir la porte de l utilisateur
+
+#code qui essaye plusieurs fois et conte les probabilité 
